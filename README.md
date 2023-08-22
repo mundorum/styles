c7ea206d038c6381c2103dcd5099cf3fd2dd24d3
André Santanchè <santanche@gmail.com>
2023-08-22 08:38:44 -0300

    feat (main): initial setup

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# styles
+# Mundorum Design
+
 Mundorum design system using design tokens and styles.
+
+## Install
+
+~~~
+npm install -D style-dictionary
+~~~
+
+## Build
+
+Generating resolved JSON and CSS files:
+~~~
+npx style-dictionary build
+~~~
+
+## Config
+
+* `config.js`
+  * `transform`: add specialized measures to the CSS output, e.g., add the `rem` measure to shape specifications;
+  * `format`: customized formats that load an extra `classes.json` file and generate:
+    * `classes/classes-resolved.json`: JSON classes with tokens transformed in their respective values;
+    * `css/variables.css`: JSON tokens transformed in CSS variables;
+    * `css/classes-tokens.css`: JSON classes transformed in CSS converting first-level properties in CSS classes and tokens to CSS variables;
+    * `css/classes-resolved.css`: same as the previous, but converting tokens in their respective values;
+    * `intermediary/aggregated-tokens.json`: an auxiliary file aggregating all tokens in a single hierarchical file.
+
+
---
+++ config.js
@@ -0,0 +1,149 @@
+// custom: to import the css transform group
+const StyleDictionary = require('style-dictionary')
+const fs = require('fs')
+const { transferableAbortController } = require('util')
+
+const jsonStrClasses =
+  fs.readFileSync('classes/classes.json').toString()
+
+// find the path in the token tree and converts to the value
+function getFinalValue(tokenName, dictionary) {
+  const tokenPath = tokenName.split('.')
+  let value = dictionary.tokens
+  let subpath = ''
+  for (const path of tokenPath) {
+    subpath += path + '.'
+    if (!value[path]) {
+      value = subpath + '..?'
+      break
+    } else {
+      value = value[path]
+    }
+  }
+  return value
+}
+
+// convert token fonts into css imports
+function cssImportFonts (dictionary) {
+  let cssFonts = ''
+  const dic = dictionary.tokens
+  if (dic.asset && dic.asset.font) {
+    const fonts = dic.asset.font
+    for (const f in fonts) {
+      const ftype =
+        (fonts[f].ttf) ? fonts[f].ttf :
+        (fonts[f].woff) ? fonts[f].woff :
+        (fonts[f].woff2) ? fonts[f].woff2 :
+        (fonts[f].eot) ? fonts[f].eot : null
+      if (ftype != null)
+        cssFonts += `@font-face {\n  font-family: '${fonts[f].name.value}';\n  src: url('${ftype.value}');\n}\n`
+    }
+    cssFonts += '\n'
+  }
+  return cssFonts
+}
+
+// convert JSON classes to CSS classes
+function classJSONtoCSS (jsonStrClasses) {
+  let cssClasses = ''
+  const jsonClasses = JSON.parse(jsonStrClasses)
+  for (const cls in jsonClasses) {
+    cssClasses += `.${cls} {\n`
+    for (const prop in jsonClasses[cls]) {
+      let value = jsonClasses[cls][prop]
+      // check Array type and transform in a string
+      if (Array.isArray(value))
+        value = value.join(' ')
+      if (value.includes('{'))
+        value = value
+          .replace(/\.value/g, '')
+          .replace(/\./g, '-')
+          .replace(/{/g, 'var(--')
+          .replace(/}/g, ')')
+      cssClasses += `  ${prop}: ${value};\n`
+    }
+    cssClasses += '}\n'
+  }
+  return cssClasses
+}
+
+/* Output 1: aggregated tokens */
+StyleDictionary.registerFormat({
+  name: 'jsonAggregatedFormat',
+  formatter: function({dictionary, platform, options, file}) {
+    return JSON.stringify(dictionary.tokens, null, 2)
+  }
+})
+
+/* Output 2: same JSON replacing tokens by final values */
+StyleDictionary.registerFormat({
+  name: 'jsonResolvedFormat',
+  formatter: function({dictionary, platform, options, file}) {
+    const jsonResClasses = jsonStrClasses
+      .replace(/{(.*)}/g, (match, tokenName) => getFinalValue(tokenName, dictionary))
+
+    return jsonResClasses
+  }
+})
+
+/* Output 3: CSS file with token variables */
+StyleDictionary.registerFormat({
+  name: 'cssClassFormat',
+  formatter: function({dictionary, platform, options, file}) {
+    return cssImportFonts(dictionary) + classJSONtoCSS (jsonStrClasses)
+  }
+})
+
+/* Output 4: CSS file with final values */
+StyleDictionary.registerFormat({
+  name: 'cssResolvedClassFormat',
+  formatter: function({dictionary, platform, options, file}) {
+    const jsonResClasses = jsonStrClasses
+      .replace(/{(.*)}/g, (match, tokenName) => getFinalValue(tokenName, dictionary))
+    return cssImportFonts(dictionary) + classJSONtoCSS (jsonResClasses)
+  }
+})
+
+module.exports = {
+  source: [`tokens/**/*.json`],
+  // custom transforms
+  transform: {
+    'shape/rem': {
+      type: 'value',
+      matcher: (token) => token.attributes.category === 'shape',
+      transformer: (token) => token.original.value + 'rem'
+    }
+  },
+  platforms: {
+    "css": {
+      "options": {
+        "showFileHeader": true
+      },
+      // extends css transform group with custom transforms
+      'transforms': StyleDictionary.transformGroup['css'].concat(['shape/rem']),
+      "buildPath": "build/",
+      "files": [
+        {
+          "format": "css/variables",
+          "destination": "css/variables.css"
+        },
+        {
+          "format": "jsonAggregatedFormat",
+          "destination": "intermediary/aggregated-tokens.json"
+        },
+        {
+          "format": "jsonResolvedFormat",
+          "destination": "classes/classes-resolved.json"
+        },
+        {
+          "format": "cssClassFormat",
+          "destination": "css/classes-tokens.css",
+        },
+        {
+          "format": "cssResolvedClassFormat",
+          "destination": "css/classes-resolved.css"
+        }
+      ]
+    }
+  }
+}
